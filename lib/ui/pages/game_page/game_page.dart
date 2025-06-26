0b45133ba6aa0bb1801d6e2866b2758355e65b98
AlfonsoDiaz09 <alfonsodiazc.09@gmail.com>
2025-06-25 19:05:12 -0600

    feat: Game page is complete

--- lib/ui/pages/game_page/game_page.dart
+++ lib/ui/pages/game_page/game_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:loteria_app/ui/pages/game_page/widgets/card_loteria.dart';
+import 'package:loteria_app/ui/pages/game_page/widgets/passed_cards.dart';
+import 'package:loteria_app/ui/pages/game_page/widgets/progress_bar_timer.dart';
 import 'package:loteria_app/ui/theme/app_theme.dart';
+import 'package:loteria_app/ui/widgets/bottom_appbar_section.dart';
 
 class GamePage extends StatelessWidget {
   const GamePage({super.key});
@@ -8,9 +12,21 @@ class GamePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: AppTheme.primary,
-        child: Center(child: Text('Game Page'))),
+      body: SafeArea(
+        child: Container(
+          color: AppTheme.primary,
+          child: Column(
+            children: [
+              PassedCards(),
+              CardLoteria(isSmallCard: false),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.bottomLeft,
+                  child: ProgressBarTimer()))
+            ]
+          )),
+      ),
+      bottomNavigationBar: BottomAppbarSection(),
     );
   }
 }
